918f45e6f5e424a1ae1a87e32778c2d6b4480e2c
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-04 18:43:08 +0530

    kafka consume springboot

---
+++ Kafka-consumer-wikimedia/src/main/java/com/springboot/kafka/SpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
